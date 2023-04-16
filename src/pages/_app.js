14a7d9d799e216fb3b253a4f4b533ef75973b12f
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-16 17:30:13 -0300

    add login page and dashboard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,13 @@
+import MainLayout from '@layout/MainLayout';
 import '../styles/tailwind.css';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
 
 export default MyApp;
---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+import LoginPage from '@components/LoadingPage';
+
+export default function Login() {
+  return <LoginPage />;
+}
