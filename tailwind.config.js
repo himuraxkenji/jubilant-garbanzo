ac2dbd19f60d8d8214dd71420bd1137d95d74331
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-16 01:02:21 -0300

    add configurations and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    colors: {
+      ...colors,
+    },
+  },
+  plugins: [],
+};
