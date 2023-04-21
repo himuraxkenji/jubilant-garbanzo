4158868d13be1d81c416c2d796037aaeb80613f9
himuraxkenji <aaron.ariperto@gmail.com>
2023-04-21 10:13:08 -0300

    add category bars

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,34 @@
 import useFetch from '@hooks/useFetch';
 import endpoints from '@services/api';
+import { Chart } from '@common/Chart';
 
 const PRODUCT_LIMIT = 5;
 const PRODUCT_OFFSET = 5;
 
 export default function Dashboard() {
+  // TODO: implement pagination
+
   const products = useFetch(endpoints.products.getProducts(PRODUCT_LIMIT, PRODUCT_OFFSET));
+
+  const categoryName = products?.map((product) => product.category);
+  const categoryCount = categoryName?.map((category) => category.name);
+
+  const countOccurences = (arr) => arr.reduce((prev, curr) => ((prev[curr] = ++prev[curr] || 1), prev), {});
+
+  const data = {
+    datasets: [
+      {
+        label: 'Categories',
+        data: countOccurences(categoryCount),
+        borderWidth: 2,
+        backgroundColor: ['#ffbb11', '#c0c0c0', '#50AF95', '#f3ba2f', '#2a71d0'],
+      },
+    ],
+  };
 
   return (
     <>
+      <Chart className="mb-8 mt-2" chartData={data} />
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
